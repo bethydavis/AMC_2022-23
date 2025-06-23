987d1323e01e667f965ca0aa8e0bb68eba7a6f6a
Beth Yima Davis <68130518+bethydavis@users.noreply.github.com>
2025-06-23 10:40:12 -1000

    update config setup

--- R/01_config.R
+++ R/01_config.R
@@ -3,8 +3,8 @@
 
 ## Identify, Install, and Load Packages 
 packages = list(
-  CRAN = c("ggplot2","dplyr", "tidyverse","rmarkdown", "beepr", "stringr"),
-  bioc = c("BiocManager", "dada2"),
+  CRAN = c("ggplot2", "vegan", "dplyr", "viridis", "ggpubr", "fossil", "paletteer", "tidyverse", "imager", "rmarkdown", "beepr", "stringr"),
+  bioc = c("BiocManager", "dada2", "phyloseq", "decontam"),
   github = c()
 )
 
